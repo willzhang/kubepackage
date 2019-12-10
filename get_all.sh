a3078c10507607377f042c1c5cf1220f3e61abb3
willgithub <524869004@qq.com>
2019-12-10 10:39:53 +0800

    Update get_all.sh

--- get_all.sh
+++ get_all.sh
@@ -3,7 +3,7 @@
 set -e
 
 sed 's/^/export /' version.list > /etc/profile.d/version.sh
-source /etc/profile.d/version.sh
+sudo source /etc/profile.d/version.sh
 
 docker run --rm --name=kubeadm-version wise2c/kubeadm-version:v${kubernetes_version} kubeadm config images list --kubernetes-version ${kubernetes_version} > ${path}/k8s-images-list.txt
 
