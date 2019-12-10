7b43bdefcb3e94d892b51cec1c2ad6587d7100d1
Your Namec <you@example.com>
2019-12-10 00:13:31 -0500

    update

--- download_all.sh
+++ download_all.sh
@@ -3,6 +3,7 @@
 set -e
 
 path=`dirname $0`
+mkdir -p ${path}/packages/{kubernetes,calico,flannel,dashboard,contour,prometheus,istio}
 
 docker run --rm --name=kubeadm-version wise2c/kubeadm-version:v${kubernetes_version} kubeadm config images list --kubernetes-version ${kubernetes_version} > ${path}/k8s-images-list.txt
 
